9c4a840f10c5c28f44d4e63cc42f84c8882f543a
qesnault <quentin.esnault@lilo.org>
2020-01-14 19:15:57 +0100

    des trucs

--- notreProjet/src/Security/UserAuthenticator.php
+++ notreProjet/src/Security/UserAuthenticator.php
@@ -79,7 +79,8 @@ class UserAuthenticator extends AbstractFormLoginAuthenticator
         // Check the user's password or other credentials and return true or false
         // If there are no credentials to check, you can just return true
         //throw new \Exception('TODO: check the credentials inside '.__FILE__);
-        return true;
+        
+        return ($user->getPassword() == $credentials['password']);
     }
 
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, $providerKey)
